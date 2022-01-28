1ef4c6da06f18da1c0a815b54e02f7a5a9b44d48
jiaqing.kuang <jiaqing.kuang@westwell-lab.com>
2022-01-28 17:44:02 +0800

    init

---
+++ src/main/java/info/westwell/spiking/filebrowser/FileBrowserEventListenerProvider.java
@@ -0,0 +1,27 @@
+package info.westwell.spiking.filebrowser;
+
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.admin.AdminEvent;
+
+public class FileBrowserEventListenerProvider implements EventListenerProvider{
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onEvent(Event arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onEvent(AdminEvent arg0, boolean arg1) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
